a861ccd1c21c8f4638e6066598fbbc15f0f73f23
Abzal <abzal008suan@gmail.com>
2022-09-28 21:41:53 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
       .setTitle("Todo backend")
